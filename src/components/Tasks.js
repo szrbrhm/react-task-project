fce9a61138a0a6fe945c2e138c05b61053ceab7d
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-08-30 12:58:00 +0200

    create tasks comp.

---
+++ src/components/Tasks.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import { initialState } from '../store/initialState';
+
+function Tasks() {
+    const [tasks, setTasks] = useState(initialState);
+    return (
+        <div>
+            {
+                tasks.map((task)) => (
+                    <h3>{task.text}</h3>
+                )
+            }
+            
+        </div>
+    )
+}
+
+export default Tasks;
