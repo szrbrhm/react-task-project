625abaf51f3aacc1078024321435b9122bd73608
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-08-30 12:43:09 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="container">
-     <Header/>
+     <Header title={"Task Tracker"}/>
     </div>
   );
 }
+
+
 
 export default App;
