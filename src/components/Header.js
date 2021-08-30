3186769c8069be26f7d4b57fb297358a6a88c689
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-08-30 12:42:56 +0200

    add btn comp.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Button from './Button/Button';
 
 
 function Header({title}) {
     return (
         <header className="header">
-            <h1>{}title</h1>
+            <h1>{title}</h1>
+            <Button color="purple" text="Show Create a Task Bar"/>
             
         </header>
     )
 }
+
+// Header.defaultProps = {
+//     title: 'Task Tracker as Default'
+//   }
+
+// Header.propTypes = {
+//     title: PropTypes.string.isRequired,
+// }
 
 export default Header
