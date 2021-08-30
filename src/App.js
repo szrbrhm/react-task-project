c797d404b847243214572a49ce6ac31db35bfac4
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-08-30 13:56:26 +0200

    add handle event

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { initialState } from "./store/initialState";
 import './App.css';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import CreateTask from "./components/CreateTask";
 
 function App() {
   const [tasks, setTasks] = useState(initialState);
 
   // Create a task
+
+  
 
   // Delete Task
   const onDelete = (deletedItemId) => setTasks(tasks.filter((task) => task.id !== deletedItemId))
@@ -19,6 +22,7 @@ function App() {
   return (
     <div className="container">
      <Header title={"Task Tracker"}/>
+     <CreateTask/>
      <Tasks tasks={tasks} onDelete={onDelete}/>
     </div>
   );
