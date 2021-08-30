05617d12c63695cda9710cf69c08cd3d96b77f52
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-08-30 13:09:06 +0200

    add map func.

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,18 +1,15 @@
-import React, { useState } from 'react'
-import { initialState } from '../store/initialState';
+import React, { useState } from "react";
+import { initialState } from "../store/initialState";
 
 function Tasks() {
-    const [tasks, setTasks] = useState(initialState);
-    return (
-        <div>
-            {
-                tasks.map((task) => (
-                    <h3 key={task.id}>{task.text}</h3>
-                ))   
-            }
-            
-        </div>
-    )
+  const [tasks, setTasks] = useState(initialState);
+  return (
+    <div>
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.text}</h3>
+      ))}
+    </div>
+  );
 }
 
 export default Tasks;
