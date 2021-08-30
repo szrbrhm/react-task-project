3782da999731594e7efb6e724ae8b812a34e1b45
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-08-30 13:30:39 +0200

    add tasks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-
+import React, { useState } from "react";
+import { initialState } from "./store/initialState";
 import './App.css';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState(initialState);
   return (
     <div className="container">
      <Header title={"Task Tracker"}/>
-     <Tasks/>
+     <Tasks tasks={tasks}/>
     </div>
   );
 }
