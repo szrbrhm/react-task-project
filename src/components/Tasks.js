cd87570ef2bd68dffaa00d6659fdd8699e9793e3
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-08-30 13:01:57 +0200

    add initialState

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -6,9 +6,9 @@ function Tasks() {
     return (
         <div>
             {
-                tasks.map((task)) => (
-                    <h3>{task.text}</h3>
-                )
+                tasks.map((task) => (
+                    <h3 key={task.id}>{task.text}</h3>
+                ))   
             }
             
         </div>
