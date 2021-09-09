4cc003e5b4f9b3cb34b7993706691e92244663a8
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-09 13:32:55 +0200

    add onCreate fun.

--- src/components/CreateTask.js
+++ src/components/CreateTask.js
@@ -1,42 +1,53 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-function CreateTask() {
-    const [text, setText] = useState("")
-    const [day, setDay] = useState("")
+function CreateTask({ onCreate }) {
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
 
-    const handleTextChange = (e) => setText(e.target.value)
-    const handleDayChange = (e) => setDay(e.target.value)
-    return (
-        <div>
-            <form className="add-form">
-      <div className="form-control">
-        <label htmlFor="task">Task</label>
-        <input
-          id="task"
-          name="text"
-          type="text"
-          placeholder="Enter a task name"
-          // required
-        //   value={text}
-          onChange={handleTextChange}
-        />
-      </div>
-      <div className="form-control">
-        <label htmlFor="day">Day & Time</label>
-        <input
-          id="day"
-          name="day"
-          type="text"
-          placeholder="Enter a day and time"
-          // required
-        //   value={day}
-          onChange={handleDayChange}
-        />
-      </div>
-      <input type="submit" value="Save Task" className="btn btn-block" />
-    </form>
+  const handleTextChange = (e) => setText(e.target.value);
+  const handleDayChange = (e) => setDay(e.target.value);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!text || !day) {
+      alert("Please fill both field");
+    } else {
+      onCreate({ text, day, isDone: false });
+      setText("");
+      setDay("");
+    }
+  };
+  return (
+    <div>
+      <form className="add-form" onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="task">Task</label>
+          <input
+            id="task"
+            name="text"
+            type="text"
+            placeholder="Enter a task name"
+            // required
+            //   value={text}
+            onChange={handleTextChange}
+          />
         </div>
-    )
+        <div className="form-control">
+          <label htmlFor="day">Day & Time</label>
+          <inp
+            id="day"
+            name="day"
+            type="text"
+            placeholder="Enter a day and time"
+            // required
+            //   value={day}
+            onChange={handleDayChange}
+          />
+        </div>
+        <input type="submit" value="Save Task" className="btn btn-block" />
+      </form>
+    </div>
+  );
 }
 
-export default CreateTask
+export default CreateTask;
